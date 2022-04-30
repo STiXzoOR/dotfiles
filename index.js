9a74a250933198ea01fbf7663f403aba46359d70
Neoptolemos Kyriakou <neostixz@gmail.com>
2022-04-30 03:20:07 +0300

    Fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const emoji = require('node-emoji')
-const packages = require('./packages/')
-const { exec } = require('promisify-child-process')
+const emoji = require("node-emoji");
+const { exec } = require("promisify-child-process");
+const packages = require("./packages/");
 
 const colorize = (color, str) => {
   const palette = {
@@ -9,41 +9,45 @@ const colorize = (color, str) => {
     green: 32,
     yellow: 33,
     cyan: 36,
-    white: 37
-  }
+    white: 37,
+  };
 
-  return `\x1b[${palette[color]}m${str}\x1b[0m`
-}
+  return `\x1b[${palette[color]}m${str}\x1b[0m`;
+};
 
 const command = (type, item) =>
   exec(
     `. scripts/echos.sh && . scripts/requirers.sh && require_${type} ${item}`,
     { cwd: __dirname }
-  )
+  );
 
 const finished = Object.keys(packages).reduce((prevTypePromise, type) => {
   return prevTypePromise.then(() => {
-    if (packages[type].length) {
-      console.log(`\n${emoji.get('coffee')} installing ${type} packages\n`)
+    console.log(`\n${emoji.get("coffee")} installing ${type} packages\n`);
 
-      return packages[type].reduce((prevItemPromise, item) => {
-        return prevItemPromise.then(() => {
-          process.stdout.write(` ⇒ ${item} `)
+    return packages[type].reduce((prevItemPromise, item) => {
+      return prevItemPromise.then(() => {
+        process.stdout.write(` ⇒ ${item} `);
 
-          return command(type, item)
-            .then(() => {
-              console.info(colorize('green', '✓'))
-            })
-            .catch(err => {
-              console.error(colorize('red', '✗'))
-              console.error(`\n  ⇒  ${err}\n`)
-            })
-        })
-      }, Promise.resolve())
-    }
-  })
-}, Promise.resolve())
+        return command(type, item)
+          .then(
+            () => {
+              console.info(colorize("green", "✓"));
+            },
+            (err) => {
+              console.error(colorize("red", "✗"));
+              console.error(`\n  ⇒  ${err}\n`);
+            }
+          )
+          .catch((err) => {
+            console.error(colorize("red", "✗"));
+            console.error(`\n  ⇒  ${err}\n`);
+          });
+      });
+    }, Promise.resolve());
+  });
+}, Promise.resolve());
 
 finished.then(() => {
-  console.log(`${emoji.get('rocket')} Package install complete.`)
-})
+  console.log(`${emoji.get("rocket")} Package install complete.`);
+});
